6cbe0fbfcb8a4ffee072c759a7da4e93e6278b12
Mohammad Shah Hosseini <7920057+mamadshni@users.noreply.github.com>
2026-05-21 15:48:53 +0200

    continue

--- src/custom-datepicker.ts
+++ src/custom-datepicker.ts
@@ -1,4 +1,5 @@
 const DATE_PICKER_VIEW_NAMES = ["day", "month", "year"] as const;
+const DEFAULT_VIEW = DATE_PICKER_VIEW_NAMES[0];
 type CustomDatepickerView = typeof DATE_PICKER_VIEW_NAMES[number];
 
 function isCustomDatePickerView(value: unknown): value is CustomDatepickerView {
@@ -8,11 +9,9 @@ function isCustomDatePickerView(value: unknown): value is CustomDatepickerView {
     return DATE_PICKER_VIEW_NAMES.includes(value as CustomDatepickerView);
 }
 
-
-
 export class CustomDatepicker extends HTMLElement {
   static get observedAttributes() {
-    return ['view', 'selectedDate', 'anchorDate'] as const;
+    return ['view'] as const;
   }
 
   constructor() {
@@ -20,47 +19,119 @@ export class CustomDatepicker extends HTMLElement {
     console.log('test');
   }
 
-  connectedCallback() {
-    const test = this.view;
-    console.log(test);
-  }
-
   attributeChangedCallback(
-    name: typeof CustomDatepicker.observedAttributes[number], 
-    oldValue: unknown, 
-    newValue: unknown
+    name: (typeof CustomDatepicker.observedAttributes)[number],
+    oldValue: unknown,
+    newValue: unknown,
   ) {
     if (oldValue === newValue) {
-        return;
+      return;
     }
 
-    this[name] = newValue;
+    switch (name) {
+      case 'view':
+        return this.#setView(newValue);
+    }
   }
+
+  #view: CustomDatepickerView = DEFAULT_VIEW;
 
   get view(): CustomDatepickerView {
-      const view = this.getAttribute('view');
-
-      return isCustomDatePickerView(view)
-        ? view
-        : (() => {
-          const result = DATE_PICKER_VIEW_NAMES[0];
-          this.view = result;
-          return result;
-        })()
+      return this.#view;
   }
 
-
-
-  set view(view: unknown) {
-    this.setAttribute(
-      'view', 
-      isCustomDatePickerView(view) ? view : DATE_PICKER_VIEW_NAMES[0]
-    );
+  set view(view: CustomDatepickerView) {
+    this.#setView(view);
   }
 
-  set selectedDate(value: unknown) {}
+  #setView(view: unknown) {
+    const next = isCustomDatePickerView(view) ? view : DEFAULT_VIEW;
 
-  set anchorDate(value: unknown) {}
+    if (this.getAttribute('view') !== next) {
+      this.setAttribute('view', next);
+    }
+
+    if (next === this.#view) return;
+    this.#view = next;
+
+    this.#render();
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('view')) {
+      this.#setView(this.getAttribute('view'));
+    } else {
+      this.setAttribute('view', this.#view);
+    }
+
+    this.#render();
+  }
+
+  #render() {
+    switch (this.#view) {
+      case 'day':
+        return this.#renderDay();
+      case 'month':
+        return this.#renderMonth();
+      case 'year':
+        return this.#renderYear();
+    }
+  }
+
+  #renderDay() {
+    const buttonContainer = document.createElement('div');
+    buttonContainer.style.display = 'flex';
+    buttonContainer.style.flexDirection = 'row';
+
+    // const month = document.createElement('button');
+    // month.innerText = 'Month';
+    // month.style.padding = '10px';
+    // month.style.backgroundColor = 'forestgreen';
+    // month.style.color = 'white';
+    // month.style.marginRight = '10px';
+
+    // const year = document.createElement('button');
+    // year.innerText = "Year";
+    // year.style.padding = '10px';
+    // year.style.backgroundColor = "magenta";
+
+    // buttonContainer.appendChild(month.cloneNode(true));
+    // buttonContainer.appendChild(year.cloneNode(true));
+
+    const main = document.createElement('div');
+    main.innerText = "Day";
+    main.style.marginTop = '30px';
+    main.style.backgroundColor = '#fbfbfb';
+
+    const button = document.createElement('button');
+    button.style.padding = '10px';
+    button.style.backgroundColor = 'magenta';
+
+    const year = button.cloneNode(true) as HTMLElement;
+    year.textContent = "Year";
+
+    const month = button.cloneNode(true) as HTMLElement;
+    month.textContent = 'Month';
+    month.style.backgroundColor = 'red';
+
+    buttonContainer.appendChild(month);
+    buttonContainer.appendChild(year);
+
+    this.innerHTML = "";
+
+    this.appendChild(buttonContainer);
+    this.appendChild(main);
+  }
+
+  #renderMonth() {
+    this.innerHTML = '';
+    this.appendChild(document.createTextNode('Month'));
+  }
+
+  #renderYear() {
+    this.innerHTML = '';
+    this.appendChild(document.createTextNode('Year'));
+  }
 }
 
 customElements.define("custom-datepicker", CustomDatepicker);
