37eb43ae71014711b61eaa92d4738b5011ffffb1
Mohammad Shah Hosseini <7920057+mamadshni@users.noreply.github.com>
2026-05-26 13:36:04 +0200

    add eventListeners

--- src/custom-datepicker.ts
+++ src/custom-datepicker.ts
@@ -10,13 +10,18 @@ function isCustomDatePickerView(value: unknown): value is CustomDatepickerView {
 }
 
 export class CustomDatepicker extends HTMLElement {
+  #abortController = new AbortController();
+
   static get observedAttributes() {
     return ['view'] as const;
   }
 
   constructor() {
     super();
-    console.log('test');
+  }
+
+  disconnectedCallback() {
+    this.#abortController.abort()
   }
 
   attributeChangedCallback(
@@ -78,25 +83,16 @@ export class CustomDatepicker extends HTMLElement {
     }
   }
 
+
+  setViewEventListner = (e: PointerEvent) => {
+    const view = (e.target as HTMLButtonElement).dataset.view;
+    this.#setView(view);
+  };
+
   #renderDay() {
     const buttonContainer = document.createElement('div');
     buttonContainer.style.display = 'flex';
     buttonContainer.style.flexDirection = 'row';
-
-    // const month = document.createElement('button');
-    // month.innerText = 'Month';
-    // month.style.padding = '10px';
-    // month.style.backgroundColor = 'forestgreen';
-    // month.style.color = 'white';
-    // month.style.marginRight = '10px';
-
-    // const year = document.createElement('button');
-    // year.innerText = "Year";
-    // year.style.padding = '10px';
-    // year.style.backgroundColor = "magenta";
-
-    // buttonContainer.appendChild(month.cloneNode(true));
-    // buttonContainer.appendChild(year.cloneNode(true));
 
     const main = document.createElement('div');
     main.innerText = "Day";
@@ -107,12 +103,22 @@ export class CustomDatepicker extends HTMLElement {
     button.style.padding = '10px';
     button.style.backgroundColor = 'magenta';
 
-    const year = button.cloneNode(true) as HTMLElement;
+    const year = button.cloneNode(true) as HTMLButtonElement;
     year.textContent = "Year";
+    year.dataset.view = 'year';
+    year.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
 
-    const month = button.cloneNode(true) as HTMLElement;
+    // year.removeEventListener('click', this.setViewEventListner)
+
+    const month = button.cloneNode(true) as HTMLButtonElement;
     month.textContent = 'Month';
     month.style.backgroundColor = 'red';
+    month.dataset.view = 'month';
+    month.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
 
     buttonContainer.appendChild(month);
     buttonContainer.appendChild(year);
@@ -124,13 +130,81 @@ export class CustomDatepicker extends HTMLElement {
   }
 
   #renderMonth() {
-    this.innerHTML = '';
-    this.appendChild(document.createTextNode('Month'));
+    const buttonContainer = document.createElement('div');
+    buttonContainer.style.display = 'flex';
+    buttonContainer.style.flexDirection = 'row';
+
+    const button = document.createElement('button');
+    button.style.padding = '10px';
+    button.style.backgroundColor = 'magenta';
+
+    const year = button.cloneNode(true) as HTMLButtonElement;
+    year.textContent = "Year";
+    year.dataset.view = 'year';
+    year.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
+    
+    const day = button.cloneNode(true) as HTMLButtonElement;
+    day.textContent = 'Day';
+    day.style.backgroundColor = 'red';
+    day.dataset.view = 'day';
+    day.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
+
+
+    const main = document.createElement('div');
+    main.innerText = "Month";
+    main.style.marginTop = '30px';
+    main.style.backgroundColor = '#fbfbfb';
+
+    buttonContainer.appendChild(day);
+    buttonContainer.appendChild(year);
+
+    this.innerHTML = "";
+
+    this.appendChild(buttonContainer);
+    this.appendChild(main);
   }
 
   #renderYear() {
-    this.innerHTML = '';
-    this.appendChild(document.createTextNode('Year'));
+    const buttonContainer = document.createElement('div');
+    buttonContainer.style.display = 'flex';
+    buttonContainer.style.flexDirection = 'row';
+
+    const month = document.createElement('button');
+    month.innerText = 'Month';
+    month.style.padding = '10px';
+    month.style.backgroundColor = 'forestgreen';
+    month.style.color = 'white';
+    month.style.marginRight = '10px';
+    month.dataset.view = 'month';
+    month.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
+
+    const day = document.createElement('button');
+    day.innerText = "Day";
+    day.style.padding = '10px';
+    day.style.backgroundColor = "magenta";
+    day.dataset.view = 'day';
+    day.addEventListener("click", this.setViewEventListner
+    , {signal: this.#abortController.signal}
+  );
+
+    buttonContainer.appendChild(month);
+    buttonContainer.appendChild(day);
+
+    const main = document.createElement('div');
+    main.innerText = "Year";
+    main.style.marginTop = '30px';
+    main.style.backgroundColor = '#fbfbfb';
+
+    this.innerHTML = "";
+
+    this.appendChild(buttonContainer);
+    this.appendChild(main);
   }
 }
 
